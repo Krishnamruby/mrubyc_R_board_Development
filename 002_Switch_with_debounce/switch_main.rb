d789e101895752b02e4c57c157e9b29477ded30c
Krishnamoorthy R <krishnamoorthy.mkr@gmail.com>
2025-05-05 14:37:55 +0530

    Swtich_with_debouncing

---
+++ 002_Switch_with_debounce/switch_main.rb
@@ -0,0 +1,16 @@
+button = GPIO.new(12, GPIO::IN)
+
+last = button.read
+
+loop do
+  current = button.read
+  if last != current
+  	if button.read == 0
+    	puts "Button Pressed"
+  	else
+    	puts "Button Released"
+	#last = current
+  	end
+  end
+  sleep(0.5)  # debounce delay
+end
